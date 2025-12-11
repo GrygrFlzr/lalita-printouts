9afc9d96d12d9c14d01047a37b563cbdf4c7a0b8
GrygrFlzr <git@cybeast.dev>
2025-12-11 14:38:25 +0700

    Reset currentPage when searching

--- src/lib/Workbook.svelte
+++ src/lib/Workbook.svelte
@@ -191,9 +191,9 @@
           <span class="font-bold">Cari data:</span>
           <input
             type="text"
-            bind:value={searchQuery}
             class="rounded-sm border border-green-500 bg-green-100 px-4 py-2 text-green-950"
-            onchange={() => {
+            bind:value={searchQuery}
+            oninput={() => {
               currentPage = 0;
             }}
           />
